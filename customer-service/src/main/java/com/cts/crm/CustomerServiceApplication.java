130628deb7751fd9e2f2b9d3eea47238837bd9e2
Syed Ehsan Ali Razvi <syed.razvi3@cognizant.com>
2020-05-05 15:17:09 +0530

    Update CustomerServiceApplication.java

--- customer-service/src/main/java/com/cts/crm/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/cts/crm/CustomerServiceApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -18,9 +16,5 @@ public class CustomerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
-	@Bean
-	   public RestTemplate getRestTemplate() {
-	      return new RestTemplate();
-	   }
 
 }
