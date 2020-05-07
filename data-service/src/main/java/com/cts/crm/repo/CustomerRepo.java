a39978eb6c1cda1b6e10ab59e14ffc072d9fdcc0
Syed Ehsan Ali Razvi <ehsan.razvi92@gmail.com>
2020-05-07 15:02:16 +0530

    asd

--- data-service/src/main/java/com/cts/crm/repo/CustomerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cts.crm.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.crm.model.Customer;
-
-public interface CustomerRepo extends JpaRepository<Customer, Integer>{
-
-}
--- scheduler-service/src/main/java/com/cts/crm/service/SchedulerService.java
+++
@@ -1,7 +0,0 @@
-package com.cts.crm.service;
-
-public interface SchedulerService {
-	
-	public void inactiveSubscription();
-
-}
